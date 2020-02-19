a061cf36ae27628a68fca9264ae2455f8bb65ada
Brandon Yum <byum1996@gmail.com>
2020-02-18 20:11:30 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import * as firebase from 'firebase';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import 'firebase/auth';
 import 'firebase/firestore';
 import { FirestoreProvider} from 'react-firestore';
 import firebaseConfig from './firebaseConfig';
 import Views from './views';
 
-firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig)
 
-function App() {
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+function App({
+  user,
+  signOut,
+  signInWithGoogle,
+}) {
   return (
     <FirestoreProvider firebase={firebase}>
       <CssBaseline />
       <Views />
+      {
+        user 
+          ? <p>Hello, {user.displayName}</p>
+          : <p>Please sign in.</p>
+      }
+      {
+        user
+          ? <button onClick={signOut}>Sign out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
     </FirestoreProvider>
   );
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
