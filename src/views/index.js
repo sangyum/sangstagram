ba99052faa0aa554c907585140ef6721cc9541ff
Sang Yum <sangyum@gmail.com>
2020-02-18 19:13:05 -0800

    Added a views

---
+++ src/views/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography'
+
+const Views = () => {
+    return (
+        <Typography variant="h1">Sangstagram</Typography>
+    )
+};
+
+export default Views;
